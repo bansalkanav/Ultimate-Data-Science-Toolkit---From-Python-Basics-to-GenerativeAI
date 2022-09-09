4ecae02c54cbebe8b3f5ab871e27059ce89736b3
Kanav Bansal <kanavbansal998@gmail.com>
2022-09-09 15:46:39 +0530

    Prod Update

--- Module 4 - Machine Learning/05. Model Productionisation/2. Application Integration/app.py
+++ Module 4 - Machine Learning/05. Model Productionisation/2. Application Integration/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 from pickle import load
 
+scaler = load(open('models/standard_scaler.pkl', 'rb'))
 lr_model = load(open('models/lr_model.pkl', 'rb'))
 
 sl = st.text_input("Sepal Length", placeholder="Enter value in cm")
@@ -13,8 +14,9 @@ btn_click = st.button("Predict")
 
 if btn_click == True:
     if sl and sw and pl and pw:
-        arr = np.array([float(sl), float(sw), float(pl), float(pw)])
-        pred = lr_model.predict(arr.reshape(1, -1))
+        query_point = np.array([float(sl), float(sw), float(pl), float(pw)]).reshape(1, -1)
+        query_point_transformed = scaler.transform(query_point)
+        pred = lr_model.predict(query_point_transformed)
         st.success(pred)
     else:
         st.error("Enter the values properly.")
