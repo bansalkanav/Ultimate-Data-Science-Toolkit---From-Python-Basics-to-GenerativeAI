6a496aa72fc2ed7048481717f08407b7071afe36
Kanav Bansal <kanavbansal998@gmail.com>
2022-09-18 17:53:48 +0530

    Prefect Update

--- Module 5 - MLOPs/4. Orchestrate ML Pipeline/version_1/workflow_v1.py
+++ Module 5 - MLOPs/4. Orchestrate ML Pipeline/version_1/workflow_v1.py
@@ -1,59 +1,60 @@
 from typing import Any, Dict, List
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-def load_data(path: str) -> pd.DataFrame:
-    return pd.read_csv(path)
-
-
-def remove_unwanted_cols(data: pd.DataFrame, unwanted_cols: List) -> pd.DataFrame:
+def load_data(path: str, unwanted_cols: List) -> pd.DataFrame:
+    data = pd.read_csv(path)
     data.drop(unwanted_cols, axis=1, inplace=True)
     return data
 
 
-def get_classes(data: pd.DataFrame, target_col: str) -> List[str]:
-    return list(data[target_col].unique())
+def get_classes(target_data: np.array) -> List[str]:
+    return list(target_data.unique())
 
 
-def rescale_numerical_columns(data: pd.DataFrame, target_col: str) -> pd.DataFrame:
-    
-    X = data.drop([target_col], axis=1)
+def rescale_numerical_columns(data: pd.DataFrame) -> pd.DataFrame:
     # scaling the numerical features
     scaler = StandardScaler()
 
     # column names are (annoyingly) lost after Scaling
     # (i.e. the dataframe is converted to a numpy ndarray)
-    data_rescaled = pd.DataFrame(scaler.fit_transform(X), 
-                                        columns = X.columns, 
-                                        index = X.index)
+    data_rescaled = pd.DataFrame(scaler.fit_transform(data), 
+                                        columns = data.columns, 
+                                        index = data.index)
 
     return data_rescaled
 
 
-def split_data(input: pd.DataFrame, output: pd.Series, test_data_ratio: float) -> Dict[str, Any]:
-   
-    X_tr, X_te, y_tr, y_te = train_test_split(input, output, test_size=test_data_ratio, random_state=0)
+def split_data(input_: pd.DataFrame, output_: pd.Series, test_data_ratio: float) -> Dict[str, Any]:
+       
+    X_tr, X_te, y_tr, y_te = train_test_split(input_, output_, test_size=test_data_ratio, random_state=0)
     
     return {'X_TRAIN': X_tr, 'Y_TRAIN': y_tr, 'X_TEST': X_te, 'Y_TEST': y_te}
 
 
-# Define Parameters
-target_col = 'Species'
-unwanted_cols = ['Id']
-test_data_ratio = 0.2
+def main():
+    # Define Parameters
+    TARGET_COL = 'Species'
+    UNWANTED_COLS = ['Id']
+    TEST_DATA_RATIO = 0.2
+    DATA_PATH = './data/iris.csv'
 
-# Run Functions
-data = load_data(path='data/iris.csv')
+    # Run Functions
+    dataframe = load_data(path=DATA_PATH, unwanted_cols=UNWANTED_COLS)
 
-# Workflow
-target_data = data[target_col]
+    # Workflow
+    target_data = dataframe[TARGET_COL]
+    input_data = dataframe.drop([TARGET_COL], axis=1)
 
-remove_unwanted_cols(data=data, unwanted_cols=unwanted_cols)
+    numerical_data = rescale_numerical_columns(data=input_data)
+     
+    classes = get_classes(target_data=target_data)
 
-numerical_data = rescale_numerical_columns(data=data, target_col=target_col)
- 
-classes = get_classes(data=data, target_col=target_col)
-
-train_test_dict = split_data(input=numerical_data, output=target_data, test_data_ratio=test_data_ratio)
+    train_test_dict = split_data(input_=numerical_data, output_=target_data, test_data_ratio=TEST_DATA_RATIO)
+    
+    
+# Run the main function
+main()
